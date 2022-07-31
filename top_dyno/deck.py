10795b97325f35caac0414cae426e6e0acaaa6d5
Richard Cândido <rich_dacan@protonmail.com>
2022-07-31 16:53:55 -0300

    refactor :hammer: refactored function to create cards based on list comprehensions

--- top_dyno/deck.py
+++ top_dyno/deck.py
@@ -7,17 +7,13 @@ DYNO_NAMES = ('Parasaurolophus', 'Stegosaurus',
 
 
 def generate_random_dynos_data() -> list[dict]:
-    dyno_cards = []
 
-    for dyno_name in DYNO_NAMES:
-
-        dyno_dict = {
+    return [
+        {
             'name': dyno_name,
             'strength': random.randint(1, 10),
             'agility': round(random.uniform(0, 10), 1),
             'height': round(random.uniform(0, 10), 2)
         }
-
-        dyno_cards.append(dyno_dict)
-
-    return dyno_cards
+        for dyno_name in DYNO_NAMES
+    ]
